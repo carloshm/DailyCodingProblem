5dee6758dd55dca9aee641003f3fd5c2302af54f
Carlos <carlos.hm@live.com>
2019-09-16 11:23:14 +0200

    challenge code 2019-16-09

---
+++ challenge-code-2019-09-16/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace challenge_code_001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("start -- challenge code 001");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Given a list of numbers and a number k, return whether any two numbers from the list add up to k.");
+            Console.WriteLine("For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.");
+
+            int[] data = new int[4] {10, 15, 3, 7};
+            int sum = 17;
+
+            Console.ForegroundColor = ConsoleColor.White;
+            foreach (int val in data){
+                if(Array.Exists(data, number => number == (sum - val))){
+                    Console.WriteLine(val);
+                }                
+            }
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("end -- challenge code 001");
+        }
+    }
+}
---
+++ challenge-code-2019-09-16/README.md
@@ -0,0 +1,8 @@
+*Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
+For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
+
+Bonus: Can you do this in one pass?*
+
+Reading this first challenge, I was playing with the numbers and the potential distribution. This brought to my mind the (triangle numbers)[https://en.wikipedia.org/wiki/Triangular_number] as the number of combinations for the array numbers would follow n(n-1)/2. In the example 4 numbers will have 6 pairs. On the other hand, it reminded me of (polygons diagonals)[https://en.wikipedia.org/wiki/Diagonal#Polygons]. You can check as well that when you connect any two vertex, you will find them follow a triangle number: diagonals = n(n-3)/2. Adding the sides you will have n(n-3)/2 + n = n(n-1)/2
+
+Looking to resolve the challenge, you can check for each element, if there is other element that match the sum.
